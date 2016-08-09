6073da81a2978ead0e72a022026c86ea71fd2902
Mikhail Navrotskiy <m.navrotskiy@hotmail.com>
2016-08-09 06:09:48 +0000

    add base methods

--- app/models/laboratory.rb
+++ app/models/laboratory.rb
@@ -1,11 +1,11 @@
 class Laboratory < ActiveLod::Base
-  attr_reader :en_name, :ru_name, :web_page, :country, :uri
+  attr_reader :en_name, :ru_name, :web_page, :country, :id
 
   def initialize(options = {})
     @en_name = options[:en_name] || ''
     @ru_name = options[:ru_name] || ''
     @web_page = options[:web_page] || ''
     @country = options[:country] || ''
-    @uri = options[:uri] || ''
+    @id = options[:id] || ''
   end
 end
--- lib/active_lod/base.rb
+++ lib/active_lod/base.rb
@@ -3,5 +3,21 @@ module ActiveLod
     URL = 'http://lod.ifmo.ru/sparql'
     
     GRAPH_URI = 'http://lod.ifmo.ru'
+    
+    def repository
+      RDF::Virtuoso::Repository.new(URL)
+    end
+    
+    def query
+      RDF::Virtuoso::Query
+    end
+    
+    def graph
+      RDF::URI.new(GRAPH_URI)
+    end
+    
+    def uri(url)
+      RDF::URI.new(url)
+    end
   end
 end

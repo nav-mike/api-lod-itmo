829573d284cbce2a44953de87c3cb1ef0169d97f
Mikhail Navrotskiy <m.navrotskiy@hotmail.com>
2016-08-09 05:39:40 +0000

    add default action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def not_found
+    respond_to do |format|
+      format.html { redirect_to 'http://rus-lod.herokuapp.com/apis' }
+      format.json do
+        render json: {error: true, message: '404. See http://rus-lod.herokuapp.com/apis'}.to_json, status: :internal_server_error
+      end
+    end
+  end
 end

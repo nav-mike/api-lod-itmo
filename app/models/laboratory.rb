3bea305b07241f6d7765f79d5a2e4604f28f4bdb
Mikhail Navrotskiy <m.navrotskiy@hotmail.com>
2016-08-09 06:17:39 +0000

    add labs all method

--- app/models/laboratory.rb
+++ app/models/laboratory.rb
@@ -8,4 +8,19 @@ class Laboratory < ActiveLod::Base
     @country = options[:country] || ''
     @id = options[:id] || ''
   end
+  
+  def self.all
+    q = query.select(:id, :en_name, :ru_name, :web_page, :counry)
+             .where(
+               [:id, uri('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'), uri('http://vivoplus.aksw.org/ontology#Laboratory')],
+               [:id, uri('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'), uri('http://vivoweb.org/ontology/core#Laboratory')],
+               [:id, uri('http://www.w3.org/2000/01/rdf-schema#label'), :en_name],
+               [:id, uri('http://www.w3.org/2000/01/rdf-schema#label'), :ru_name],
+               [:id, uri('http://www.w3.org/2000/01/rdf-schema#seeAlso'), :web_page],
+               [:id, uri('http://vivoplus.aksw.org/ontology#locatedIn'), :country]
+             ).filter('lang(?en_name) = "en"').filter('lang(?ru_name) = "ru"')
+             .distinct.graph(graph)
+
+    repository.select(q)
+  end
 end

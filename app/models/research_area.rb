b19f4df51107761f280bb693cad7d119f9aaaa73
Mikhail Navrotskiy <m.navrotskiy@hotmail.com>
2016-08-09 18:36:44 +0000

    add research area all method

--- app/models/research_area.rb
+++ app/models/research_area.rb
@@ -5,4 +5,24 @@ class ResearchArea < ActiveLod::Base
     @name = options[:name] || ''
     @id = options[:id] || ''
   end
+  
+  def self.all
+    query = sparql.select(:id, :name).distinct
+      .where(
+        [:id, RDF::URI('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'), RDF::URI('http://vivoplus.aksw.org/ontology#ResearchArea')],
+        [:id, RDF::URI('http://www.w3.org/2000/01/rdf-schema#label'), :name]
+      )
+    
+    result = []
+    query.each_solution do |solution|
+      result << to_research_area(solution)
+    end
+  end
+  
+  def self.to_research_area(solution)
+    rec = {
+      id: solution[:id].value,
+      name: solution[:name].value
+    }
+  end
 end

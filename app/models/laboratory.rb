e48883dbba60976db10edc638282cf391ac3d611
Mikhail Navrotskiy <m.navrotskiy@hotmail.com>
2016-08-09 05:58:24 +0000

    add laboratory model

---
+++ app/models/laboratory.rb
@@ -0,0 +1,11 @@
+class Laboratory
+  attr_reader :en_name, :ru_name, :web_page, :country, :uri
+
+  def initialize(options = {})
+    @en_name = options[:en_name] || ''
+    @ru_name = options[:ru_name] || ''
+    @web_page = options[:web_page] || ''
+    @country = options[:country] || ''
+    @uri = options[:uri] || ''
+  end
+end

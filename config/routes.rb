ceef8f9a19c61f3501a6d97b7508340adc3ef820
Mikhail Navrotskiy <m.navrotskiy@hotmail.com>
2016-08-16 06:08:40 +0000

    #1 add controller action

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :laboratories, only: %i(index show)
   resources :people, only: %i(index show)
   
+  get '/laboratories/:laboratory_id/people' => 'people#lab_index', constraints: {laboratory_id: /[0-9]+/, format: /json/}
+  
   root 'application#not_found'
   
   get '*unmatched_route', to: 'application#not_found'

9b489e14270e1687e55e1478968a0d7def57e88d
Mikhail Navrotskiy <m.navrotskiy@hotmail.com>
2016-08-16 06:57:25 +0000

    #1 add model request

--- app/models/person.rb
+++ app/models/person.rb
@@ -13,6 +13,28 @@ class Person < ActiveLod::Base
     @pcard_id = options[:pcard_id] || ''
   end
   
+  def self.find_by_laboratory(id)
+    query = sparql.select(:id, :academic_qualification, :academic_status, :affiliated_organization,
+                          :first_name, :last_name, :middle_name, :pcard_id).distinct
+      .where(
+        [:id, RDF::URI('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'), RDF::URI('http://xmlns.com/foaf/0.1/Person')],
+        [:id, RDF::URI('http://vivoweb.org/ontology/core#affiliatedOrganization'), :affiliated_organization]
+      )
+      .optional([:id, RDF::URI('http://vivoplus.aksw.org/ontology#academicQualification'), :academic_qualification])
+      .optional([:id, RDF::URI('http://xmlns.com/foaf/0.1/firstName'), :first_name])
+      .optional([:id, RDF::URI('http://xmlns.com/foaf/0.1/lastName'), :last_name])
+      .optional([:id, RDF::URI('http://vivoweb.org/ontology/core#middleName'), :middle_name])
+      .optional([:id, RDF::URI('http://vivoplus.aksw.org/ontology#pcardId'), :pcard_id])
+      .optional([:id, RDF::URI('http://vivoplus.aksw.org/ontology#academicStatus'), :academic_status])
+      .filter("STR(?affiliated_organization)=\"http://lod.ifmo.ru/Laboratory#{id}\"")
+      
+    result = []
+    query.each_solution do |solution|
+      result << to_person(solution)
+    end
+    result
+  end
+  
   def self.find(id)
     query = sparql.select(:id, :academic_qualification, :academic_status, :affiliated_organization,
                           :first_name, :last_name, :middle_name, :pcard_id)
@@ -53,15 +75,21 @@ class Person < ActiveLod::Base
   end
   
   def self.to_person(solution)
+    aq = solution[:academic_qualification].present? ? solution[:academic_qualification].value : ''
+    as = solution[:academic_status].present? ? solution[:academic_status].value : ''
+    fn = solution[:first_name].present? ? solution[:first_name].value : ''
+    ln = solution[:last_name].present? ? solution[:last_name].value : ''
+    mn = solution[:middle_name].present? ? solution[:middle_name].value : ''
+    pi = solution[:pcard_id].present? ? solution[:pcard_id].value : ''
     rec = {
       id: solution[:id].value,
-      academic_qualification: solution[:academic_qualification].value,
-      academic_status: solution[:academic_status].value,
+      academic_qualification: aq,
+      academic_status: as,
       affiliated_organization: solution[:affiliated_organization].value,
-      first_name: solution[:first_name].value,
-      last_name: solution[:last_name].value,
-      middle_name: solution[:middle_name].value,
-      pcard_id: solution[:pcard_id].value
+      first_name: fn,
+      last_name: ln,
+      middle_name: mn,
+      pcard_id: pi
     }
     Person.new rec
   end

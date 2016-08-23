8ae6dfb007552a039566203861830ff869493ad6
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2016-08-23 21:22:05 +0000

    add course validation rules

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,9 +1,9 @@
 class Application < ActiveRecord::Base
   validates :name, uniqueness: true
   validates :name, length: {in: 2..32}
-  
+
   validates :description, length: {in: 0..200}
-  
+
   validates :link, length: {in: 0..100}
 
   validates :key, uniqueness: true
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,9 @@
 class Course < ActiveRecord::Base
+  validates :name, length: {in: 2..100}
+
+  validates :description, length: {in: 0..200}
+
+  validates :link, length: {in: 0..100}
+
+  validates :teacher, length: {in: 100}
 end
